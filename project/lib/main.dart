17a52f416cf9b49713e0fcbad409928651650324
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2023-12-12 00:05:50 +0100

    Updated main

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loading_bar_widget/widgets/pass_strenght_meter_widget.dart';
+import 'package:loading_bar_widget/widgets/progress_bar_widget.dart';
+import 'package:loading_bar_widget/widgets/stepper_widget.dart';
 
 void main() {
   runApp(const AppTest());
@@ -13,7 +15,6 @@ class AppTest extends StatefulWidget {
 }
 
 class AppTestState extends State<AppTest> {
-  int currentValue = 20;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,23 +35,18 @@ class AppTestState extends State<AppTest> {
                         fontSize: 20,
                         color: Colors.white,
                         decoration: TextDecoration.none))),
-            Expanded(child: PasswordStrMeterWidget()),
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: PasswordStrMeterWidget()),
+            const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text("Steps lefts",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        decoration: TextDecoration.none))),
+            StepperWidget(steps: ["Order", "Sent", "Arrived"], currentStep: 1)
           ],
-        ))
-        // const Text("PROGRESS BAR"),
-        // ProgressBarWidget(
-        //   progressDirection: ProgressDirection.rightToLeft,
-        //     maxValue: 100,
-        //   barHeight: 20,
-        //   currentValue: currentValue,
-        // ),
-        // ElevatedButton(onPressed: (){
-        //   setState(() {
-        //     currentValue+=10;
-        //   });
-        // }, child: Text("Incr moi wesh")),
-        // const Text("Steps left"),
-        // StepsLeftWidget(steps: const ["Order placed", "In review", "Approval", "End"], currentStep: 3)
-        );
+        )));
   }
 }
